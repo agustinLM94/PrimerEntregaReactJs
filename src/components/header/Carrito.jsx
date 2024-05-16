28ffe5e728ec491c07651de28c70407db5bfccdc
aguslopez <agustin.emanuel.lopez@gmail.com>
2024-05-15 21:24:23 -0300

    hola

---
+++ src/components/header/Carrito.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Carrito = () => {
+  return (
+    <div className="carrito">
+    🛒 1
+    </div>  
+  )
+}
+
+export default Carrito
---
+++ src/components/header/NavBar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Carrito from './Carrito'
+
+const NavBar = () => {
+  return (
+    
+        <nav className='nav'>
+            <ul className='nav-menu'>
+                <li><a className='nav-link 'href="#">Inicio</a></li>
+                <li><a className='nav-link 'href="#">Alimentos</a></li>
+                <li><a className='nav-link 'href="#">Juguetes</a></li>
+                <li><a className='nav-link 'href="#">Medicamentos</a></li>
+            </ul>
+        </nav>
+        
+    
+    
+  )
+}
+
+export default NavBar
